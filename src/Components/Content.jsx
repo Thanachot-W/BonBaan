b5cf7a1ed873204708d4c28f1225d06e574fea63
Thanachot-W <thanachot.wongy@mail.kmutt.ac.th>
2025-01-23 23:57:12 +0700

    logo 2

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Content = () => {
   return (
     <div className="mt-6 bg-white p-6 rounded-lg shadow-md">
-      <p className="text-gray-600">
+      <p className="text-gray-600 font-notosans">
         ยินดีต้อนรับสู่ระบบ BONBAAN คุณสามารถจัดการคำสั่งซื้อ บริการ และอื่น ๆ ได้ที่นี่
       </p>
     </div>
