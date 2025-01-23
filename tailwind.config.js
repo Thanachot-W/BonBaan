978dd8906d860390dcefee49967688d8cdcbe667
FC4RICA <kantinan.fw@gmail.com>
2025-01-24 03:44:28 +0700

    Add daisyui theme and custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,25 @@ export default {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "--b1": "96.23% 0.0121 296.35",
+          "--p": "48.23% 0.2694 285.02",
+          "--border": "84.95% 0.0125 296.31"
+        },
+      }
+    ]
+  },
 }
 
