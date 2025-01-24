07669ae20c55174daa5523307a61c9b01f9ce1b8
Thanachot-W <thanachot.wongy@mail.kmutt.ac.th>
2025-01-24 10:51:21 +0700

    sidebar

--- src/Components/SidebarItem.jsx
+++ src/Components/SidebarItem.jsx
@@ -4,7 +4,7 @@ const SidebarItem = ({ text, icon, active }) => {
   return (
     <li
       className={`flex items-center px-6 py-3 space-x-3 text-lg rounded-lg ${
-        active ? "bg-purple-300 text-[#5E17EB] " : "hover:bg-[#5E17EB] cursor-pointer"
+        active ? " bg-purple-300 text-[#5E17EB] " : "hover:bg-[#c6bcff] hover:mr-2 hover:ml-2 hover:text-[#5e17eb] cursor-pointer"
       }`}
     >
       <span className="material-icons">{icon}</span>
