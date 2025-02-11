8db22bcf35f287fa786d93aa2fed80a438b73df7
FC4RICA <kantinan.fw@gmail.com>
2025-02-11 16:21:25 +0700

    feat: Add order cancellation form using dialog component

---
+++ src/components/forms/orderForm.jsx
@@ -0,0 +1,71 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cancelOrderSchema } from "../../schemas/orderSchema";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+const CancelOrderForm = ({ onSubmit, closeDialog, children }) => {
+  const form = useForm({
+    resolver: zodResolver(cancelOrderSchema),
+    defaultValues: {
+      header: "",
+      description: ""
+    },
+  });
+
+  const handelSubmit = (values) => {
+    if (onSubmit) {
+      onSubmit(values);
+    }
+    if (closeDialog) {
+      closeDialog();
+    }
+    console.log(values);
+    
+    // call api
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handelSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="header"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>เหตุผลการปฎิเสธคำสั่งซื้อ</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="หัวข้อเหตุผล"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>รายละเอียดการปฎิเสธคำสั่งซื้อ</FormLabel>
+              <FormControl>
+                <Textarea {...field} placeholder="รายละเอียด"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {children}
+      </form>
+    </Form>
+  );
+};
+
+export { CancelOrderForm }
--- src/components/layout/OrderCard.jsx
+++ src/components/layout/OrderCard.jsx
@@ -6,6 +6,18 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { CancelOrderForm } from "../forms/orderForm";
+import { useState } from "react";
 
 const OrderCard = ({ data, onConfirm, onCancel }) => {
   return (
@@ -17,16 +29,68 @@ const OrderCard = ({ data, onConfirm, onCancel }) => {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <p>{data.name} ต้องการ {data.type} ที่ {data.service}</p>
+        <p>
+          {data.name} ต้องการ {data.type} ที่ {data.service}
+        </p>
         <p>{data.detail}</p>
       </CardContent>
       <CardFooter className="flex gap-2 justify-end">
-        <Button className="px-8" onClick={onConfirm}>ยืนยัน</Button>
-        <Button className="px-8" variant="destructive" onClick={onCancel}>
-          ปฏิเสธ
-        </Button>
+        <ConfirmDialog
+          onSubmit={onConfirm}
+          trigger={<Button className="px-8">ยืนยัน</Button>}
+        />
+        <CancelDialog
+          onSubmit={onCancel}
+          trigger={
+            <Button className="px-8" variant="destructive">
+              ปฏิเสธ
+            </Button>
+          }
+        />
       </CardFooter>
     </Card>
+  );
+};
+
+const ConfirmDialog = ({ onSubmit, trigger }) => (
+  <Dialog>
+    <DialogTrigger asChild>{trigger}</DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>กรุณาใส่ข้อมูลเพื่อยืนยันคำสั่งซื้อ</DialogTitle>
+        <DialogDescription>กรุณากำหนดราคาสำหรับคำสั่งซื้อ</DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+);
+
+const CancelDialog = ({ onSubmit, trigger }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>กรุณาใส่ข้อมูลเพื่อยกเลิกคำสั่งซื้อ</DialogTitle>
+          <DialogDescription>
+            กรุณาเลือกเหตุผลการปฎิเสธคำสั่งซื้อ
+            การปฎิเสธคำสั่งซื้ออาจทำให้ลูกค้าไม่พึงพอใจ
+            ด้งนั้นกรุณาใส่เหตุผลประกอบเพิ่มเติม
+          </DialogDescription>
+        </DialogHeader>
+        <CancelOrderForm closeDialog={() => setOpen(false)} onSubmit={onSubmit}>
+          <DialogFooter>
+            <Button type="submit">ยืนยัน</Button>
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                ยกเลิก
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </CancelOrderForm>
+      </DialogContent>
+    </Dialog>
   );
 };
 
