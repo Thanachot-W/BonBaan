e56149d0dd3de78e7b7e83cdbca09f0cd699e072
Thanachot-W <thanachot.wongy@mail.kmutt.ac.th>
2025-02-13 19:38:37 +0700

    feature-orderspage-v3

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState("null");
+  const [user, setUser] = useState(null);
 
   return(
     <UserContext.Provider value={{user, setUser}}>
